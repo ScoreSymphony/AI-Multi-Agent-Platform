8dbe6b54663ef1aee8be24415fe46cf720f9cc0b
Score Symphony <scoresymphony@gmail.com>
2026-09-13 16:31:56 +0200

    test: make evaluation approval reader awaitable (#892)

--- tests/test_issue_19_behavior_evidence.py
+++ tests/test_issue_19_behavior_evidence.py
@@ -54,7 +54,7 @@ class ApprovalRecords:
     def __init__(self, records: tuple[ApprovalRecord, ...]) -> None:
         self.records = records
 
-    def all(self) -> tuple[ApprovalRecord, ...]:
+    async def all(self) -> tuple[ApprovalRecord, ...]:
         return self.records
 
 
