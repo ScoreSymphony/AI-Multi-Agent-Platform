2c2ddc62870a1073a85e56b83eec9d59bc5caf97
Score Symphony <scoresymphony@gmail.com>
2026-09-16 00:01:46 +0200

    Import workspace staging token helper from codec

--- tests/unit/distributed/test_workspace_transport_codec_path_budget.py
+++ tests/unit/distributed/test_workspace_transport_codec_path_budget.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import hashlib
 
-from ai_multi_agent_platform.distributed.workspace_transport import _entry_token, _ManifestEntry
+from ai_multi_agent_platform.distributed.workspace_transport_codec import _entry_token, _ManifestEntry
 
 
 def _entry(relative_path: str) -> _ManifestEntry:
