8004cab97eb2750997345319a03aa62302a4e588
Score Symphony <scoresymphony@gmail.com>
2026-09-16 23:45:30 +0200

    style(applications): organize SQLite test imports

--- tests/unit/applications/test_sqlite_repository.py
+++ tests/unit/applications/test_sqlite_repository.py
@@ -7,8 +7,8 @@ import pytest
 
 from ai_multi_agent_platform.applications.definition import Application
 from ai_multi_agent_platform.applications.models import (
-    ApplicationConfigValueType,
     ApplicationConfigurationField,
+    ApplicationConfigValueType,
     ApplicationDesiredState,
     ApplicationEndpoint,
     ApplicationEndpointExposure,
