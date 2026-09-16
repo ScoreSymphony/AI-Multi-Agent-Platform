2ab45cc17217769ef67c0d2394f1e3f28d4062d2
Score Symphony <scoresymphony@gmail.com>
2026-09-16 12:39:59 +0200

    Polish organizations collaboration administration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,6 +12,7 @@ import "./evaluations.css";
 import "./approvals.css";
 import "./governance.css";
 import "./notifications.css";
+import "./organizations.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Missing #root element");
