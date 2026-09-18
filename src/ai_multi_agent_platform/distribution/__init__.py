fbfcfb137b44158aefcabde5df40b4b9c43f66c0
Score Symphony <scoresymphony@gmail.com>
2026-09-18 09:09:19 +0200

    feat: export marketplace decision contracts

--- src/ai_multi_agent_platform/distribution/__init__.py
+++ src/ai_multi_agent_platform/distribution/__init__.py
@@ -17,6 +17,19 @@ from .control_plane import (
     RegistryValidationContextResolver,
     register_distribution_control_plane,
 )
+from .decision import (
+    ApprovalRequirement,
+    CompatibilityDecision,
+    DependencyResolution,
+    DependencyStatus,
+    DistributionOperation,
+    MarketplaceDecision,
+    PermissionDiff,
+    ProvenanceDiff,
+    UpdateState,
+    evaluate_compatibility,
+    resolve_dependency_graph,
+)
 from .discovery import (
     CuratedCandidateReview,
     DiscoveryCandidate,
@@ -37,6 +50,7 @@ from .local import LocalRegistryProvider
 from .models import (
     ArtifactIntegrity,
     DistributionRoute,
+    RegistryCompatibility,
     RegistryDependency,
     RegistryItemKind,
     RegistryItemType,
@@ -48,14 +62,26 @@ from .models import (
     registry_item_kind_value,
 )
 from .plugin_adapter import PluginRegistryArtifactInstaller
-from .provider import RegistryItemNotFoundError, RegistryProvider, RegistryUnavailableError
+from .provider import (
+    MultiRegistryProvider,
+    RegistryItemNotFoundError,
+    RegistryProvider,
+    RegistrySourceConflictError,
+    RegistryUnavailableError,
+    SourcedRegistryProvider,
+)
 from .reconciliation import RegistryPluginReconciliationError, reconcile_registry_plugins
 from .schema import (
     REGISTRY_ITEM_SCHEMA_VERSION,
     registry_item_from_document,
     validate_registry_item_document,
 )
-from .service import DistributionPreview, DistributionRouter, DistributionService
+from .service import (
+    DistributionPreview,
+    DistributionRouter,
+    DistributionService,
+    DistributionUninstallPreview,
+)
 from .signatures import (
     HmacSha256SignatureVerifier,
     RegistrySignatureVerifier,
@@ -79,6 +105,7 @@ from .technical_catalog import (
     is_technical_component,
 )
 from .validation import (
+    FindingCategory,
     FindingSeverity,
     ValidationContext,
     ValidationFinding,
@@ -87,6 +114,23 @@ from .validation import (
 )
 
 __all__ = [
+    "resolve_dependency_graph",
+    "evaluate_compatibility",
+    "UpdateState",
+    "SourcedRegistryProvider",
+    "RegistrySourceConflictError",
+    "RegistryCompatibility",
+    "ProvenanceDiff",
+    "PermissionDiff",
+    "MultiRegistryProvider",
+    "MarketplaceDecision",
+    "FindingCategory",
+    "DistributionUninstallPreview",
+    "DistributionOperation",
+    "DependencyStatus",
+    "DependencyResolution",
+    "CompatibilityDecision",
+    "ApprovalRequirement",
     "ArtifactIntegrity",
     "BUILTIN_MARKETPLACE_KINDS",
     "CanonicalDistributionRouter",
