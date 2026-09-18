88bf0d7c38f62c9e4f7479a9e01658271a0a64b9
Score Symphony <scoresymphony@gmail.com>
2026-09-19 01:36:47 +0200

    test: keep one canonical security conformance matrix

--- src/ai_multi_agent_platform/conformance/__init__.py
+++ src/ai_multi_agent_platform/conformance/__init__.py
@@ -16,11 +16,6 @@ from ai_multi_agent_platform.conformance.optional_profiles import (
     activate_optional_scenarios,
     optional_evidence_ids,
 )
-from ai_multi_agent_platform.conformance.security_boundaries import (
-    SecurityBoundaryClaim,
-    security_boundary_claims,
-    security_boundary_pytest_nodes,
-)
 
 __all__ = [
     "REPORT_SCHEMA",
@@ -31,11 +26,8 @@ __all__ = [
     "ConformanceScenario",
     "ConformanceScenarioResult",
     "ConformanceStatus",
-    "SecurityBoundaryClaim",
     "activate_optional_scenarios",
     "optional_evidence_ids",
     "profile_scenarios",
     "run_conformance",
-    "security_boundary_claims",
-    "security_boundary_pytest_nodes",
 ]
