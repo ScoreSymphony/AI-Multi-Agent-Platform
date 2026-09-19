5fbc0d866917b6f6e9cfc7f5f42c2cc843e50cfa
Score Symphony <scoresymphony@gmail.com>
2026-09-19 03:57:42 +0200

    style: apply ruff format to src/ai_multi_agent_platform/control_plane/ha_composition.py

--- src/ai_multi_agent_platform/control_plane/ha_composition.py
+++ src/ai_multi_agent_platform/control_plane/ha_composition.py
@@ -12,8 +12,12 @@ from ai_multi_agent_platform.distributed.high_availability.contracts import (
     ControlPlaneRole,
     CoordinationError,
 )
-from ai_multi_agent_platform.distributed.high_availability.integrations import AuthorityGatedAutomationLoop
-from ai_multi_agent_platform.distributed.high_availability.service import ControlPlaneFailoverService
+from ai_multi_agent_platform.distributed.high_availability.integrations import (
+    AuthorityGatedAutomationLoop,
+)
+from ai_multi_agent_platform.distributed.high_availability.service import (
+    ControlPlaneFailoverService,
+)
 
 from .approval_portability_composition import ControlPlane as _BaseControlPlane
 from .models import RequestContext
